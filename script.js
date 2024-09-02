9bcb222e3da3c34f33c6dd0c4b42f94d519c3eab
gawhite92 <122578880+gawhite92@users.noreply.github.com>
2024-09-03 07:52:35 +0800

    Refactored constructor to use class

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ const myLibrary = [
 
 //console.log(myLibrary[1])
 
-function Book(title, author, pages, read) {
-    this.title = title,
+class Book {
+    constructor(title,author,pages,read) {
+        this.title = title,
         this.author = author,
         this.pages = pages,
         this.read = read
+    }
 }
 
 function showNewBookForm() {
